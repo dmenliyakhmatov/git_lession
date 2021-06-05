44072865254d17870e325e51825a3f3b0fce9dbe
Dmitriy Menliyakhmatov <menliiakhmatovdg@lad24.ru>
2021-06-05 13:16:35 +0300

    commiteedd

---
+++ Delegation/demo1.js
@@ -0,0 +1,25 @@
+let list = document.querySelector('.list');
+const arr = [];
+for (let i = 1; i <= 8; i++) {
+  arr.push(i);
+}
+const listItem = document.createElement('li');
+listItem.classList.add('list-item');
+
+// arr.forEach((item) => {
+//   const newItem = listItem.cloneNode();
+//   newItem.textContent = item;
+//   list.append(newItem);
+// });
+
+const clickHanlder = (evt) => {
+  if (evt.target.className === 'list-item') {
+    alert(evt.target.innerText);
+  }
+};
+
+document.body.addEventListener('click', clickHanlder);
+
+function sayHi() {
+  return 'Hello';
+}
